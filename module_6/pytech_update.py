78bf377b3cbba86f3b8937f3555f1f934d27c9b8
John Hadden <john.t.hadden@gmail.com>
2021-11-21 20:45:45 -0500

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -4,4 +4,8 @@ url = "mongodb+srv://admin:admin@cluster0.jiuf0.mongodb.net/pytech?retryWrites=t
 client = MongoClient(url)
 db = client.pytech
 
-db.students.find()
+print(db.students.find())
+
+result = db.students.update_one({“student_id”: 1007}, {“$set”: {“last_name”: “Hadden II”}})
+
+doc = db.collection_name.find_one({“student_id”: “1007”})
