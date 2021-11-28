802682c18c85ce81788c206c2fab25b678ed08d4
John Hadden <john.t.hadden@gmail.com>
2021-11-28 13:13:37 -0500

    Create pysports_queries.py

---
+++ module_8/pysports_queries.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host", confid["database"]]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid.")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    db.close()
+    
+SELECT team_id, team_name, mascot FROM team;
+
+cursor = db.cursor()
+cursor.execute(“SELECT team_id, team_name, mascot FROM team”)
+teams = cursor.fetchall()
+for team in teams:
+  print(“Team Name: {}”.format(team[1]))
+
+SELECT player_id, first_name, last_name FROM team;
+
+cursor = db.cursor()
+cursor.execute(“SELECT player_id, first_name, last_name FROM team”)
+players = cursor.fetchall()
+for player in players:
+  print(“Player Name: {}”.format(player[1]))
